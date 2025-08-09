5794c4f5cb189bf34bd3a7a7b5bad498527ebcc1
EduardaAndrade03 <eduardaamaria2608@gmail.com>
2025-08-08 21:05:30 -0300

    Add files via upload

--- soma5.py
+++ soma5.py
@@ -1,8 +1,9 @@
-numero1 =int(input("me de um numero" ))
-numero2 =int(input("me da outro" ))
-numero3 =int(input("me da outro" ))
-numero4 =int(input("me da outro" ))
-numero5 =int(input("me da outro" ))
-n= numero1 + numero2 + numero3 + numero4 +numero5
-print(n)
-1
+
+soma=0
+num=int(input("fala um numero"))
+for n in range(4):
+    soma+=num
+    num=int(input("diz outro"))
+    
+soma+=num
+print(soma)
