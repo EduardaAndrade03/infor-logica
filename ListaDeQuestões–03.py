76d59383d64409153335df5760d558f2aa09cf5e
EduardaAndrade03 <eduardaamaria2608@gmail.com>
2025-08-21 09:06:47 -0300

    Add files via upload

---
+++ ListaDeQuestões–03.py
@@ -0,0 +1,27 @@
+# # Questâo 1   
+
+# peso=float(input("informe seu peso em kilos "))
+# altura=float(input("informe sua altura em metros "))
+# imc= peso/(altura*altura)
+# print(f"Seu imc é {imc}")
+# if imc< 18.5:
+#     print("Abaixo do Peso")
+# elif imc>= (18.5) and imc <= (24.9):
+#     print("Peso normal")
+# elif imc >=25 and imc<= 29.9:
+#     print("Sobrepeso")
+# elif imc>=30:
+#     # print("Obeso")
+
+
+# Questâo 2
+
+n = int(input("digite um numero "))
+for num in range(1, n+1):
+    if num%2 ==0:
+        print(f"{num} é par")
+    else:
+        print(f"{num} é impar")
+
+
+
